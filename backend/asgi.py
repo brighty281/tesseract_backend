88d99511fa50d8ca5dd013f55c7cbeb394f01d98
brighty281 <brightythomas281@gmail.com>
2024-07-27 20:39:38 +0530

    asgi edit

--- backend/asgi.py
+++ backend/asgi.py
@@ -8,20 +8,33 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 from django.urls import path
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import AuthMiddlewareStack
 from chats.routes import websocket_urlpatterns
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django.setup()
+
+from chats.routes import websocket_urlpatterns  # Ensure this import is after django.setup()
+from channels.security.websocket import AllowedHostsOriginValidator
+
+# django_asgi_app = get_asgi_application()
+# application = ProtocolTypeRouter({
+
+#     "http": django_asgi_app,
+#     "websocket":AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+
+#     })
 
 django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
 
     "http": django_asgi_app,
-    "websocket":AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
-
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(URLRouter(websocket_urlpatterns))
+        )
     })
-
 
