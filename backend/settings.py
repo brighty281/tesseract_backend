49417de1f50b67df1532869029ece9a943f1f0fb
brighty281 <brightythomas281@gmail.com>
2024-07-29 19:21:38 +0530

    reddis

--- backend/settings.py
+++ backend/settings.py
@@ -87,21 +87,21 @@ ASGI_APPLICATION = 'backend.asgi.application'
 
 
 # Channel layers
-# CHANNEL_LAYERS = {
-#     'default': {
-#         'BACKEND': 'channels_redis.core.RedisChannelLayer',
-#         'CONFIG': {
-#             "hosts": [('127.0.0.1', 6379)],  # Ensure this matches your Redis server's host and port
-#         },
-#     },
-# }
-
-
 CHANNEL_LAYERS = {
-    'default':{
-        'BACKEND':'channels.layers.InMemoryChannelLayer'
-    }
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],  # Ensure this matches your Redis server's host and port
+        },
+    },
 }
+
+
+# CHANNEL_LAYERS = {
+#     'default':{
+#         'BACKEND':'channels.layers.InMemoryChannelLayer'
+#     }
+# }
 
 
 # Database
@@ -164,7 +164,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 # Default primary key field type
